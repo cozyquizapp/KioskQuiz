4c5f3b8947ab641efc60a30467e03795eb86192d
WolfJohannes93 <johanneswolf993@gmail.com>
2026-02-07 16:07:34 +0100

    refactor: remove legacy and backup files (249KB cleanup)

--- frontend/src/components/beamer/index.legacy.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BeamerScoreboardCard = exports.BeamerBadge = exports.BeamerFrame = void 0;
-var BeamerFrame_1 = require("./BeamerFrame");
-Object.defineProperty(exports, "BeamerFrame", { enumerable: true, get: function () { return BeamerFrame_1.default; } });
-var BeamerBadge_1 = require("./BeamerBadge");
-Object.defineProperty(exports, "BeamerBadge", { enumerable: true, get: function () { return BeamerBadge_1.default; } });
-var BeamerScoreboardCard_1 = require("./BeamerScoreboardCard");
-Object.defineProperty(exports, "BeamerScoreboardCard", { enumerable: true, get: function () { return BeamerScoreboardCard_1.default; } });
